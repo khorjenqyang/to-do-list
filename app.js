a13716b8ecca7a57578068162a3aa7b697561a89
JenqYang Khor <JenqYang_Khor@jabil.com>
2023-03-08 08:36:38 +0800

    hello

---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser:true })
+
+// define schema
+const itemsSchema = new mongoose.Schema ({
+    name: String
+})
+
+// create model
+const Item = mongoose.model("Item",itemsSchema)
+
+const item1 = new Item({
+   name: "Welcome to your todolist!"
+})
+
+const item2 = new Item({
+   name: "Hit the + button to add a new item"
+})
+
+const item3 = new Item({
+   name: "<-- Hit this to delete an item"
+})
+
+const defaultItems = [item1,item2,item3]
+
+
+app.get("/", function(req, res) {
+
+  // get all documents from mongoose db and render
+  Item.find({}).then(foundItems => {
+
+    if (foundItems.length === 0){
+      //insert
+      Item.insertMany(defaultItems).then(function(){
+        console.log("Data inserted")  // Success
+      }).catch(function(error){
+        console.log(error)      // Failure
+      });
+      res.redirect("/") // because redirect again will not fall into this condition
+
+    } else{
+
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+
+    }
+
+  
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  
+});
+
+app.post("/", function(req, res){
+
+  const itemName = req.body.newItem;
+  
+  const item = new Item({
+    name: itemName
+ })
+
+  item.save()
+
+  res.redirect("/");
+
+});
+
+app.post("/delete",function(req,res){
+
+  console.log(req.body)
+
+})
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
